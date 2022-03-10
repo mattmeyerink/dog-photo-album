c6e0b85f3df0be01eb893c7e4e23861cf5897b29
mattmeyerink <meyerink@umich.edu>
2022-03-09 19:07:38 -0800

    create utility methods

---
+++ src/utils.ts
@@ -0,0 +1,33 @@
+/**
+ * Returns a promise that resolves to true after the passed number of ms
+ * @param ms number
+ * @returns Promise
+ */
+function delay(ms: number) {
+    return new Promise((res) => setTimeout(() => res(true), ms));
+}
+
+/**
+ * Move the passed element right 100px over 1 second
+ * @param el HTMLElement
+ */
+function animateRight(el: HTMLElement) {
+    let elementOffset = 0;
+    const animationInterval = setInterval(() => {
+        elementOffset += 1;
+        el.style.left = `${elementOffset}px`;
+
+        if (elementOffset >= 100) {
+            clearInterval(animationInterval);
+        }
+    }, 10);
+}
+
+/**
+ * Removes duplicates from an input array.
+ * @param xs any[]
+ * @returns any[]
+ */
+function removeDuplicates(xs: any[]) {
+    return Array.from(new Set(xs))
+}
