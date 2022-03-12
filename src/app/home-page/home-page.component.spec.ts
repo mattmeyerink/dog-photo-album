d316f88fdc555d5cd63b1bb7ca2c16a59a73db19
mattmeyerink <meyerink@umich.edu>
2022-03-12 10:09:28 -0800

    fix incorrect mocking of fetch requests

--- src/app/home-page/home-page.component.spec.ts
+++ src/app/home-page/home-page.component.spec.ts
@@ -1,18 +1,15 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { HomePageComponent } from './home-page.component';
 
 describe('HomePageComponent', () => {
   let component: HomePageComponent;
   let fixture: ComponentFixture<HomePageComponent>;
-
-  const unmockedFetch = globalThis.fetch;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomePageComponent ]
+      declarations: [HomePageComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -21,33 +18,21 @@ describe('HomePageComponent', () => {
     fixture.detectChanges();
   });
 
-  afterAll(() => {
-    globalThis.fetch = unmockedFetch;
-  });
-
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should successfully fetch the random dog image', async () => {
-    globalThis.fetch = () =>
-      Promise.resolve({
-        json: () => Promise.resolve({ message: 'testURL', status: 'success' }),
-      } as Response);
-
+  it('should successfully fetch the random dog image', waitForAsync(async () => {
+    spyOn(window, 'fetch').and.returnValue(Promise.resolve({ json: async () => ({ message: 'testURL', status: 'success' }) } as unknown as Response));
     component.dogImage = '';
     await component.fetchDogImage();
     expect(component.dogImage).toEqual('testURL');
-  });
+  }));
 
-  it('should handle errored dog image fetch', async () => {
-    globalThis.fetch = () =>
-      Promise.resolve({
-        json: () => Promise.resolve({ message: 'testURL', status: 'error' }),
-      } as Response);
-
+  it('should handle errored dog image fetch', waitForAsync(async () => {
+    spyOn(window, 'fetch').and.returnValue(Promise.resolve({ json: async () => ({ message: 'errorURL', status: 'error' }) } as unknown as Response));
     component.dogImage = '';
     await component.fetchDogImage();
     expect(component.dogImage).toEqual('');
-  });
+  }));
 });
