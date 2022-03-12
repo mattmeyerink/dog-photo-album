45a268a29449645a6b43d96003764a9fd29abe8b
mattmeyerink <meyerink@umich.edu>
2022-03-12 09:31:31 -0800

    add tests for home page component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('dog-photo-album');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('dog-photo-album app is running!');
-  });
 });
--- src/app/home-page/home-page.component.spec.ts
+++ src/app/home-page/home-page.component.spec.ts
@@ -6,6 +6,8 @@ describe('HomePageComponent', () => {
   let component: HomePageComponent;
   let fixture: ComponentFixture<HomePageComponent>;
 
+  const unmockedFetch = globalThis.fetch;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ HomePageComponent ]
@@ -19,7 +21,33 @@ describe('HomePageComponent', () => {
     fixture.detectChanges();
   });
 
+  afterAll(() => {
+    globalThis.fetch = unmockedFetch;
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should successfully fetch the random dog image', async () => {
+    globalThis.fetch = () =>
+      Promise.resolve({
+        json: () => Promise.resolve({ message: 'testURL', status: 'success' }),
+      } as Response);
+
+    component.dogImage = '';
+    await component.fetchDogImage();
+    expect(component.dogImage).toEqual('testURL');
+  });
+
+  it('should handle errored dog image fetch', async () => {
+    globalThis.fetch = () =>
+      Promise.resolve({
+        json: () => Promise.resolve({ message: 'testURL', status: 'error' }),
+      } as Response);
+
+    component.dogImage = '';
+    await component.fetchDogImage();
+    expect(component.dogImage).toEqual('');
   });
 });
