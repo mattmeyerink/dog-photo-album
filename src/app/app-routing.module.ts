38ef9ff2236b6ed619e2db01f2f4887a3433e1b4
mattmeyerink <meyerink@umich.edu>
2022-03-11 19:51:34 -0800

    add routing logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { MoreDogPhotosComponent } from './more-dog-photos/more-dog-photos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'more-dogs', component: MoreDogPhotosComponent },
+  { path: 'home', component: HomePageComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
